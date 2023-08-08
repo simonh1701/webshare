208addc247b521db8f80fea839a9e587b4b7f577
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-08-08 07:07:03 +0200

    Fix color of message component

--- components/message.tsx
+++ components/message.tsx
@@ -1,5 +1,7 @@
 import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/20/solid";
 
+import cn from "@/util/classnames";
+
 type Props = {
   message: string;
   type: MessageType;
@@ -15,24 +17,35 @@ export default function Message({ message, type }: Props) {
 
   return (
     <div
-      className={`mb-4 border-[1px] border-l-8 border-${color} bg-${color}/10 p-4`}
+      className={cn(
+        "mb-4 border-[1px] border-l-8 p-4",
+        type === MessageType.Error
+          ? "border-error bg-error/10"
+          : "border-success bg-success/10"
+      )}
     >
       <div className="flex">
         <div className="flex flex-shrink-0 flex-row justify-center">
           {type === MessageType.Error ? (
             <XCircleIcon
-              className={`my-auto h-5 w-5 text-${color}`}
+              className="my-auto h-5 w-5 text-error"
               aria-hidden="true"
             />
           ) : (
             <CheckCircleIcon
-              className={`my-auto h-5 w-5 text-${color}`}
+              className="my-auto h-5 w-5 text-success"
               aria-hidden="true"
             />
           )}
         </div>
         <div className="ml-3">
-          <p className={`text-${color}`}>{message}</p>
+          <p
+            className={
+              type === MessageType.Error ? "text-error" : "text-success"
+            }
+          >
+            {message}
+          </p>
         </div>
       </div>
     </div>
