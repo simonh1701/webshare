624517c0110e93d89632157c5690ee2e9725b35c
Simon Hohenwarter <113617179+simonh1701@users.noreply.github.com>
2023-09-30 07:47:26 +0200

    Improve SEO and add Favicon and Open Graph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,28 @@ export const metadata = {
   title: "Webshare",
   description:
     "Securely share text snippets over the internet. Basically a web clipboard.",
+  openGraph: {
+    title: "Webshare",
+    description:
+      "Securely share text snippets over the internet. Basically a web clipboard.",
+    url: "https://webshare.simonh1701.app",
+    siteName: "Webshare",
+    locale: "en",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    noimageindex: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": 0,
+      "max-image-preview": "none",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
